b48c8ee2ab2787d72519f4fbf967f98c24553f12
alegian <alegiannx@gmail.com>
2025-03-16 15:29:42 +0200

    clean up matrix a tiny bit

--- src/main/java/me/alegian/thavma/impl/common/block/MatrixBlock.kt
+++ src/main/java/me/alegian/thavma/impl/common/block/MatrixBlock.kt
@@ -5,21 +5,21 @@ import me.alegian.thavma.impl.init.registries.deferred.T7BlockEntities
 import net.minecraft.core.BlockPos
 import net.minecraft.world.InteractionResult
 import net.minecraft.world.entity.player.Player
+import net.minecraft.world.level.BlockGetter
 import net.minecraft.world.level.Level
 import net.minecraft.world.level.block.Block
 import net.minecraft.world.level.block.Blocks
 import net.minecraft.world.level.block.EntityBlock
 import net.minecraft.world.level.block.RenderShape
-import net.minecraft.world.level.block.entity.BlockEntity
 import net.minecraft.world.level.block.state.BlockState
 import net.minecraft.world.level.material.PushReaction
 import net.minecraft.world.phys.BlockHitResult
 
-class MatrixBlock : Block(Properties.ofFullCopy(Blocks.STONE).noOcclusion().pushReaction(PushReaction.BLOCK)),
-    EntityBlock {
+private val animKeys = listOf("closed", "open", "spin_open", "spin_closed", "spin_closed_fast")
+
+class MatrixBlock : Block(Properties.ofFullCopy(Blocks.STONE).noOcclusion().pushReaction(PushReaction.BLOCK)), EntityBlock {
     companion object {
-        val animKeys = listOf("closed", "open", "spin_open", "spin_closed", "spin_closed_fast")
-        var cycle = 0
+        var debugCycle = 0
     }
 
     override fun useWithoutItem(
@@ -32,17 +32,15 @@ class MatrixBlock : Block(Properties.ofFullCopy(Blocks.STONE).noOcclusion().push
         if (!pLevel.isClientSide()) pLevel.getBlockEntity(pPos, T7BlockEntities.MATRIX.get()).ifPresent {
             it.triggerAnim(
                 "cycle",
-                animKeys[cycle++ % animKeys.size]
+                animKeys[debugCycle++ % animKeys.size]
             )
         }
         return InteractionResult.SUCCESS
     }
 
-    override fun getRenderShape(state: BlockState): RenderShape {
-        return RenderShape.ENTITYBLOCK_ANIMATED
-    }
+    override fun getRenderShape(state: BlockState) = RenderShape.ENTITYBLOCK_ANIMATED
 
-    override fun newBlockEntity(pos: BlockPos, blockState: BlockState): BlockEntity {
-        return MatrixBE(pos, blockState)
-    }
+    override fun newBlockEntity(pos: BlockPos, blockState: BlockState) = MatrixBE(pos, blockState)
+
+    override fun propagatesSkylightDown(state: BlockState, level: BlockGetter, pos: BlockPos) = true
 }

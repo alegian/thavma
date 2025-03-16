5ea9b720547cb619d74b6e11eb483dfe37e700d9
alegian <alegiannx@gmail.com>
2025-03-16 15:20:58 +0200

    pedestal BER

---
+++ src/main/java/me/alegian/thavma/impl/client/renderer/blockentity/PedestalBER.kt
@@ -0,0 +1,47 @@
+package me.alegian.thavma.impl.client.renderer.blockentity
+
+import com.mojang.blaze3d.vertex.PoseStack
+import com.mojang.blaze3d.vertex.VertexConsumer
+import com.mojang.math.Axis
+import me.alegian.thavma.impl.client.renderer.geo.layer.EmissiveGeoLayer
+import me.alegian.thavma.impl.client.util.scale
+import me.alegian.thavma.impl.common.block.entity.PedestalBE
+import me.alegian.thavma.impl.common.util.use
+import me.alegian.thavma.impl.rl
+import net.minecraft.client.Minecraft
+import net.minecraft.client.renderer.MultiBufferSource
+import net.minecraft.client.renderer.RenderType
+import net.minecraft.world.item.ItemDisplayContext
+import net.minecraft.world.item.Items
+import software.bernie.geckolib.cache.`object`.BakedGeoModel
+import software.bernie.geckolib.model.DefaultedBlockGeoModel
+import software.bernie.geckolib.renderer.GeoBlockRenderer
+
+// item rotation speed
+private const val DEGREES_PER_TICK = 5
+
+class PedestalBER : GeoBlockRenderer<PedestalBE>(DefaultedBlockGeoModel<PedestalBE>(rl("infusion_pedestal"))) {
+  init {
+    this.addRenderLayer(EmissiveGeoLayer<PedestalBE>(this))
+  }
+
+  override fun actuallyRender(
+    poseStack: PoseStack, be: PedestalBE, model: BakedGeoModel, renderType: RenderType?,
+    bufferSource: MultiBufferSource, buffer: VertexConsumer?, isReRender: Boolean, partialTick: Float, packedLight: Int,
+    packedOverlay: Int, colour: Int
+  ) {
+    super.actuallyRender(poseStack, be, model, renderType, bufferSource, buffer, isReRender, partialTick, packedLight, packedOverlay, colour)
+
+    val itemRenderer = Minecraft.getInstance().itemRenderer
+    val level = be.level ?: return
+
+    val randSeed = be.blockPos.asLong().toInt()
+
+    poseStack.use {
+      translate(0.0, 1.0, 0.0)
+      scale(0.8f)
+      mulPose(Axis.YP.rotationDegrees(((level.gameTime + partialTick) * DEGREES_PER_TICK) % 360))
+      itemRenderer.renderStatic(Items.DIAMOND.defaultInstance, ItemDisplayContext.GROUND, packedLight, packedOverlay, poseStack, bufferSource, level, randSeed)
+    }
+  }
+}
--- src/main/java/me/alegian/thavma/impl/common/block/PedestalBlock.kt
+++ src/main/java/me/alegian/thavma/impl/common/block/PedestalBlock.kt
@@ -2,21 +2,24 @@ package me.alegian.thavma.impl.common.block
 
 import me.alegian.thavma.impl.common.block.entity.PedestalBE
 import net.minecraft.core.BlockPos
+import net.minecraft.world.level.BlockGetter
 import net.minecraft.world.level.block.Block
+import net.minecraft.world.level.block.Block.box
 import net.minecraft.world.level.block.Blocks
 import net.minecraft.world.level.block.EntityBlock
 import net.minecraft.world.level.block.RenderShape
-import net.minecraft.world.level.block.entity.BlockEntity
 import net.minecraft.world.level.block.state.BlockState
 import net.minecraft.world.level.material.PushReaction
+import net.minecraft.world.phys.shapes.CollisionContext
 
-class PedestalBlock : Block(Properties.ofFullCopy(Blocks.STONE).noOcclusion().pushReaction(PushReaction.BLOCK)),
-    EntityBlock {
-    override fun getRenderShape(state: BlockState): RenderShape {
-        return RenderShape.ENTITYBLOCK_ANIMATED
-    }
+private val SHAPE = box(5.0, 0.0, 5.0, 11.0, 13.0, 11.0)
 
-    override fun newBlockEntity(pos: BlockPos, blockState: BlockState): BlockEntity {
-        return PedestalBE(pos, blockState)
-    }
+class PedestalBlock : Block(Properties.ofFullCopy(Blocks.STONE).noOcclusion().pushReaction(PushReaction.BLOCK)), EntityBlock {
+    override fun getShape(state: BlockState, level: BlockGetter, pos: BlockPos, context: CollisionContext) = SHAPE
+
+    override fun getRenderShape(state: BlockState) = RenderShape.ENTITYBLOCK_ANIMATED
+
+    override fun newBlockEntity(pos: BlockPos, blockState: BlockState) = PedestalBE(pos, blockState)
+
+    override fun propagatesSkylightDown(state: BlockState, level: BlockGetter, pos: BlockPos) = true
 }

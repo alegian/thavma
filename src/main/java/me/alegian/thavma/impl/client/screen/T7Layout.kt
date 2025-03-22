bebc1573a8f891730ed4abca0c91a9606873c295
alegian <alegiannx@gmail.com>
2025-03-22 15:38:20 +0200

    prep for next stage

--- src/main/java/me/alegian/thavma/impl/client/screen/T7Layout.kt
+++ src/main/java/me/alegian/thavma/impl/client/screen/T7Layout.kt
@@ -1,6 +1,7 @@
 package me.alegian.thavma.impl.client.screen
 
 import net.minecraft.client.gui.components.Renderable
+import org.joml.Vector2i
 import kotlin.math.max
 
 private var currParent: T7LayoutElement? = null
@@ -30,8 +31,12 @@ private fun T7Layout(
   element.children()
   currParent = element.parent
 
-  element.calculateSizes()
+  // first pass: reverse BFS
+  element.calculateInitialSizes()
   if (element.parent == null) {
+    // second pass: DFS from root
+    element.calculateDynamicSizesRecursively()
+    // third pass: DFS from root
     element.calculatePositionsRecursively()
     currParent = null
   }
@@ -39,10 +44,33 @@ private fun T7Layout(
 }
 
 class Position(var x: Int = 0, var y: Int = 0)
-class Size(var width: Int = 0, var height: Int = 0)
+class Size(x: Int = 0, y: Int = 0) : Vector2i(x, y) {
+  constructor(size: Vector2i) : this(size.x, size.y)
+
+  operator fun plusAssign(other: Vector2i) {
+    add(other)
+  }
+
+  operator fun minusAssign(other: Vector2i) {
+    sub(other)
+  }
+}
+
 class Padding(val left: Int = 0, val right: Int = 0, val top: Int = 0, val bottom: Int = 0) {
   constructor(all: Int) : this(all, all, all, all)
   constructor(x: Int, y: Int) : this(x, x, y, y)
+
+  fun start(direction: Direction): Vector2i {
+    if (direction == Direction.LEFT_RIGHT || direction == Direction.TOP_BOTTOM) return Vector2i(left, top)
+    return Vector2i(0, 0)
+  }
+
+  fun end(direction: Direction): Vector2i {
+    if (direction == Direction.LEFT_RIGHT || direction == Direction.TOP_BOTTOM) return Vector2i(right, bottom)
+    return Vector2i(0, 0)
+  }
+
+  val all = Vector2i(left + right, top + bottom)
 }
 
 class T7LayoutElement(
@@ -59,20 +87,29 @@ class T7LayoutElement(
     parent?.children?.add(this)
   }
 
-  // first pass: sizing
-  fun calculateSizes() {
-    size.width += padding.left + padding.right
-    size.height += padding.top + padding.bottom
+  // first pass
+  fun calculateInitialSizes() {
+    size += padding.all
     val childGaps = gap * (children.size - 1)
     addSizeAlong(direction.axis, childGaps)
 
     if (parent == null) return
     val parentAxis = parent.direction.axis
-    parent.addSizeAlong(parentAxis, getSizeAlong(parentAxis))
-    parent.maxSizeAlong(parentAxis?.cross(), getSizeAlong(parentAxis?.cross()))
+    parent.size += getSizeAlong(parentAxis)
+    parent.size = max(parent.size, getSizeAlong(parentAxis?.cross()))
   }
 
-  // second pass: positioning
+  // second pass
+  fun calculateDynamicSizesRecursively() {
+    val remainingSize = Size(size)
+    remainingSize -= padding.all
+
+    for (child in children) {
+      remainingSize -= child.getSizeAlong(direction.axis)
+    }
+  }
+
+  // third pass
   // TODO: support reverse directions
   fun calculatePositionsRecursively() {
     var xOffset = position.x + padding.left
@@ -82,9 +119,9 @@ class T7LayoutElement(
       child.position.x = xOffset
       child.position.y = yOffset
       if (direction == Direction.LEFT_RIGHT)
-        xOffset += child.size.width + gap
+        xOffset += child.size.x + gap
       else if (direction == Direction.TOP_BOTTOM)
-        yOffset += child.size.height + gap
+        yOffset += child.size.y + gap
 
       child.calculatePositionsRecursively()
     }
@@ -93,34 +130,25 @@ class T7LayoutElement(
   // helper
   fun addSizeAlong(axis: Axis?, dx: Int) {
     if (axis == Axis.HORIZONTAL) {
-      size.width += dx
+      size.x += dx
     } else if (axis == Axis.VERTICAL) {
-      size.height += dx
+      size.y += dx
     }
   }
 
   // helper
-  fun maxSizeAlong(axis: Axis?, dx: Int) {
+  fun getSizeAlong(axis: Axis?): Size {
     if (axis == Axis.HORIZONTAL) {
-      size.width = max(size.width, dx)
+      return Size(size.x, 0)
     } else if (axis == Axis.VERTICAL) {
-      size.height = max(size.height, dx)
+      return Size(0, size.y)
     }
-  }
-
-  // helper
-  fun getSizeAlong(axis: Axis?): Int {
-    if (axis == Axis.HORIZONTAL) {
-      return size.width
-    } else if (axis == Axis.VERTICAL) {
-      return size.height
-    }
-    return 0
+    return Size(0, 0)
   }
 
   // helper
   fun debugRect(color: Int) = Renderable { guiGraphics, _, _, _ ->
-    guiGraphics.fill(position.x, position.y, position.x + size.width, position.y + size.height, color)
+    guiGraphics.fill(position.x, position.y, position.x + size.x, position.y + size.y, color)
   }
 }
 
@@ -147,3 +175,5 @@ fun Box(
   gap: Int = 0,
   children: T7LayoutElement.() -> Unit
 ) = T7Layout(position, size, padding, Direction.NONE, gap, children)
+
+fun max(a: Size, b: Size) = Size(max(a.x, b.x), max(a.y, b.y))

a83208c3b12c3d476441fbccc94f1379436ee980
alegian <alegiannx@gmail.com>
2025-06-21 15:09:27 +0300

    lots of datagen

--- src/main/java/me/alegian/thavma/impl/init/data/providers/aspects/MineralAspects.kt
+++ src/main/java/me/alegian/thavma/impl/init/data/providers/aspects/MineralAspects.kt
@@ -6,14 +6,18 @@ import me.alegian.thavma.impl.init.data.providers.item
 import me.alegian.thavma.impl.init.registries.T7DataMaps
 import me.alegian.thavma.impl.init.registries.deferred.Aspects.DESIDERIUM
 import me.alegian.thavma.impl.init.registries.deferred.Aspects.IGNIS
+import me.alegian.thavma.impl.init.registries.deferred.Aspects.INSTRUMENTUM
 import me.alegian.thavma.impl.init.registries.deferred.Aspects.LUX
 import me.alegian.thavma.impl.init.registries.deferred.Aspects.METALLUM
 import me.alegian.thavma.impl.init.registries.deferred.Aspects.PERMUTATIO
 import me.alegian.thavma.impl.init.registries.deferred.Aspects.POTENTIA
+import me.alegian.thavma.impl.init.registries.deferred.Aspects.PRAECANTATIO
 import me.alegian.thavma.impl.init.registries.deferred.Aspects.SENSUS
 import me.alegian.thavma.impl.init.registries.deferred.Aspects.TENEBRAE
 import me.alegian.thavma.impl.init.registries.deferred.Aspects.TERRA
 import me.alegian.thavma.impl.init.registries.deferred.Aspects.VITREUS
+import me.alegian.thavma.impl.init.registries.deferred.T7Blocks
+import me.alegian.thavma.impl.init.registries.deferred.T7Items
 import net.minecraft.core.HolderLookup
 import net.minecraft.tags.ItemTags
 import net.minecraft.world.item.Items
@@ -23,19 +27,62 @@ import net.neoforged.neoforge.common.Tags
 object MineralAspects {
   fun gather(datamapProvider: T7DataMapProvider, lookupProvider: HolderLookup.Provider) {
     datamapProvider.builder(T7DataMaps.AspectContent.BLOCK).run {
-      block(Blocks.GLOWSTONE) {
-        it.add(LUX.get(), 16)
-          .add(TERRA.get(), 4)
-      }
       block(Tags.Blocks.STORAGE_BLOCKS_COAL) {
         it.add(METALLUM.get(), 144)
           .add(DESIDERIUM.get(), 72)
           .add(TENEBRAE.get(), 36)
       }
-      block(Blocks.NETHERITE_BLOCK) {
+      block(Tags.Blocks.STORAGE_BLOCKS_COPPER) {
+        it.add(METALLUM.get(), 36)
+          .add(PERMUTATIO.get(), 36)
+      }
+      block(Tags.Blocks.STORAGE_BLOCKS_IRON) {
+        it.add(METALLUM.get(), 72)
+      }
+      block(T7Blocks.THAVMITE_BLOCK.get()) {
+        it.add(METALLUM.get(), 72)
+          .add(PRAECANTATIO.get(), 36)
+      }
+      block(T7Blocks.ORICHALCUM_BLOCK.get()) {
+        it.add(METALLUM.get(), 36)
+          .add(PRAECANTATIO.get(), 36)
+      }
+      block(Tags.Blocks.STORAGE_BLOCKS_GOLD) {
+        it.add(METALLUM.get(), 36)
+          .add(DESIDERIUM.get(), 72)
+      }
+      block(Tags.Blocks.STORAGE_BLOCKS_DIAMOND) {
+        it.add(VITREUS.get(), 72)
+          .add(DESIDERIUM.get(), 72)
+      }
+      block(Tags.Blocks.STORAGE_BLOCKS_EMERALD) {
+        it.add(VITREUS.get(), 72)
+          .add(DESIDERIUM.get(), 36)
+      }
+      block(Tags.Blocks.STORAGE_BLOCKS_NETHERITE) {
         it.add(METALLUM.get(), 144)
           .add(DESIDERIUM.get(), 72)
           .add(TENEBRAE.get(), 36)
+      }
+      block(Blocks.GLOWSTONE) {
+        it.add(LUX.get(), 16)
+          .add(TERRA.get(), 4)
+      }
+      block(Blocks.QUARTZ_BLOCK) {
+        it.add(VITREUS.get(), 16)
+          .add(IGNIS.get(), 4)
+      }
+      block(Tags.Blocks.STORAGE_BLOCKS_LAPIS) {
+        it.add(VITREUS.get(), 36)
+          .add(SENSUS.get(), 18)
+      }
+      block(Tags.Blocks.STORAGE_BLOCKS_REDSTONE) {
+        it.add(POTENTIA.get(), 36)
+          .add(TERRA.get(), 9)
+      }
+      block(Blocks.AMETHYST_BLOCK) {
+        it.add(VITREUS.get(), 16)
+          .add(SENSUS.get(), 4)
       }
     }
 
@@ -50,6 +97,14 @@ object MineralAspects {
       }
       item(Tags.Items.INGOTS_IRON) {
         it.add(METALLUM.get(), 8)
+      }
+      item(T7Items.THAVMITE_INGOT.get()) {
+        it.add(METALLUM.get(), 8)
+          .add(PRAECANTATIO.get(), 4)
+      }
+      item(T7Items.ORICHALCUM_INGOT.get()) {
+        it.add(METALLUM.get(), 4)
+          .add(INSTRUMENTUM.get(), 4)
       }
       item(Tags.Items.INGOTS_GOLD) {
         it.add(METALLUM.get(), 4)
@@ -78,8 +133,8 @@ object MineralAspects {
           .add(TERRA.get(), 1)
       }
       item(Tags.Items.GEMS_QUARTZ) {
-        it.add(IGNIS.get(), 1)
-          .add(VITREUS.get(), 4)
+        it.add(VITREUS.get(), 4)
+          .add(IGNIS.get(), 1)
       }
       item(Tags.Items.GEMS_LAPIS) {
         it.add(VITREUS.get(), 4)

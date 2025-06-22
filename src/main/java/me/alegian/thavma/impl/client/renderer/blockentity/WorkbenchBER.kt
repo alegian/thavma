1d5bd6f617e313d57a59c3ad2a70d7eef1951936
alegian <alegiannx@gmail.com>
2025-06-22 19:25:47 +0300

    workbench glowmask

--- src/main/java/me/alegian/thavma/impl/client/renderer/blockentity/WorkbenchBER.kt
+++ src/main/java/me/alegian/thavma/impl/client/renderer/blockentity/WorkbenchBER.kt
@@ -1,8 +1,13 @@
 package me.alegian.thavma.impl.client.renderer.blockentity
 
+import me.alegian.thavma.impl.client.renderer.geo.layer.EmissiveGeoLayer
 import me.alegian.thavma.impl.common.block.entity.WorkbenchBE
 import me.alegian.thavma.impl.rl
 import software.bernie.geckolib.model.DefaultedBlockGeoModel
 import software.bernie.geckolib.renderer.GeoBlockRenderer
 
-class WorkbenchBER : GeoBlockRenderer<WorkbenchBE>(DefaultedBlockGeoModel(rl("arcane_workbench")))
+class WorkbenchBER : GeoBlockRenderer<WorkbenchBE>(DefaultedBlockGeoModel(rl("arcane_workbench"))){
+  init {
+    addRenderLayer(EmissiveGeoLayer(this))
+  }
+}

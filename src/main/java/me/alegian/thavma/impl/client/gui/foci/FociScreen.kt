c95df36189f1123988f13d6c34cde92d79fa636e
alegian <alegiannx@gmail.com>
2025-06-25 15:52:18 +0300

    render some random items on the wheel

--- src/main/java/me/alegian/thavma/impl/client/gui/foci/FociScreen.kt
+++ src/main/java/me/alegian/thavma/impl/client/gui/foci/FociScreen.kt
@@ -8,18 +8,45 @@ import me.alegian.thavma.impl.client.util.*
 import net.minecraft.client.gui.GuiGraphics
 import net.minecraft.client.gui.screens.Screen
 import net.minecraft.network.chat.Component
+import net.minecraft.world.item.Items
+import kotlin.math.PI
+import kotlin.math.cos
+import kotlin.math.sin
 
 private const val TITLE_ID = "screen." + Thavma.MODID + ".title"
 private val BACKGROUND = Texture("gui/foci/circle", 236, 236)
+private const val SCALE = 0.5
 
 class FociScreen : Screen(Component.translatable(TITLE_ID)) {
+  var lastMouseX = width / 2
+  var lastMouseY = height / 2
+  val options = listOf(Items.DIAMOND.defaultInstance, Items.DIAMOND_PICKAXE.defaultInstance, Items.PUFFERFISH.defaultInstance, Items.TURTLE_HELMET.defaultInstance)
+
   override fun renderBackground(guiGraphics: GuiGraphics, mouseX: Int, mouseY: Int, partialTick: Float) {
     guiGraphics.usePose {
       translateXY(width / 2, height / 2)
-      setRenderSystemColor(T7Colors.PURPLE)
-      guiGraphics.blitCentered(BACKGROUND)
-      resetRenderSystemColor()
+
+      guiGraphics.usePose {
+        scaleXY(SCALE)
+        setRenderSystemColor(T7Colors.PURPLE)
+        guiGraphics.blitCentered(BACKGROUND)
+        resetRenderSystemColor()
+      }
+
+      translateXY(-8, -8)
+      for ((i, stack) in options.withIndex()) {
+        val radius = SCALE * BACKGROUND.width / 2
+        val angle = 2 * PI / options.size
+        guiGraphics.renderItem(
+          stack,
+          (sin(angle * i) * radius).toInt(),
+          (cos(angle * i) * radius).toInt()
+        )
+      }
     }
+    
+    lastMouseX = mouseX
+    lastMouseY = mouseY
   }
 
   override fun keyReleased(keyCode: Int, scanCode: Int, modifiers: Int): Boolean {

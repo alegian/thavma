4dd62cefcf628e573b750dfa3a8e5a2fc0e725b4
alegian <alegiannx@gmail.com>
2025-08-16 10:52:21 +0300

    basic light focus

---
+++ src/main/java/me/alegian/thavma/impl/common/item/LightFocus.kt
@@ -0,0 +1,19 @@
+package me.alegian.thavma.impl.common.item
+
+import me.alegian.thavma.impl.init.registries.deferred.T7Blocks
+import net.minecraft.world.InteractionResult
+import net.minecraft.world.item.BlockItem
+import net.minecraft.world.item.Item
+import net.minecraft.world.item.context.BlockPlaceContext
+import net.minecraft.world.item.context.UseOnContext
+
+class LightFocus: Item(
+  Properties().stacksTo(1)
+) {
+  override fun useOn(context: UseOnContext): InteractionResult {
+    val blockItem = T7Blocks.ETERNAL_FLAME.get().asItem()
+    if(blockItem !is BlockItem) return InteractionResult.PASS
+
+    return blockItem.place(BlockPlaceContext(context))
+  }
+}

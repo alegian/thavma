92f0609ba46c10b0a5d5e70aeff5d1d69b303b63
alegian <alegiannx@gmail.com>
2025-08-30 17:21:16 +0300

    cleaner crafting page

--- src/main/java/me/alegian/thavma/impl/client/gui/book/CraftingPageRenderer.kt
+++ src/main/java/me/alegian/thavma/impl/client/gui/book/CraftingPageRenderer.kt
@@ -3,11 +3,9 @@ package me.alegian.thavma.impl.client.gui.book
 import me.alegian.thavma.impl.client.gui.layout.*
 import me.alegian.thavma.impl.client.texture.Texture
 import me.alegian.thavma.impl.client.util.drawCenteredString
-import me.alegian.thavma.impl.client.util.drawString
 import me.alegian.thavma.impl.client.util.translateXY
 import me.alegian.thavma.impl.client.util.usePose
 import me.alegian.thavma.impl.common.book.CraftingPage
-import me.alegian.thavma.impl.common.book.TextPage
 import me.alegian.thavma.impl.common.recipe.translationId
 import net.minecraft.client.Minecraft
 import net.minecraft.client.gui.components.Renderable
@@ -18,37 +16,40 @@ import kotlin.jvm.optionals.getOrNull
 
 object CraftingPageRenderer : PageRenderer<CraftingPage> {
   private val GRID = Texture("gui/book/crafting", 96, 96, 96, 96)
+  private val RESULT = Texture("gui/book/result", 32, 32, 32, 32)
   private val TITLE = Component.translatable(RecipeType.CRAFTING.translationId)
+  private const val GAP = 12
 
   override fun initPage(screen: EntryScreen, page: CraftingPage) {
     val recipe = Minecraft.getInstance().level?.recipeManager?.byKey(page.recipeRL)?.getOrNull()?.value
     if (recipe !is CraftingRecipe) return
 
     Column({
+      alignCross = Alignment.CENTER
       size = grow()
-      gap = 4
+      gap = GAP
     }) {
       Title(screen)
 
-      Row({
-        width = grow()
-        alignMain = Alignment.CENTER
-      }) {
-        Row({
-          width = fixed(GRID.width)
-          height = fixed(GRID.height)
-        }) {
-          afterLayout {
-            screen.addRenderableOnly(texture(GRID))
-          }
-        }
+      Image(screen, RESULT)
+
+      Image(screen, GRID)
+    }
+  }
+
+  private fun Image(screen: EntryScreen, texture: Texture) {
+    Row({
+      width = fixed(texture.width)
+      height = fixed(texture.height)
+    }) {
+      afterLayout {
+        screen.addRenderableOnly(texture(texture))
       }
     }
   }
 
   private fun Title(screen: EntryScreen) {
     Row({
-      width = grow()
       height = fixed(screen.getFont().lineHeight)
     }) {
       afterLayout {
--- src/main/java/me/alegian/thavma/impl/common/item/BookItem.kt
+++ src/main/java/me/alegian/thavma/impl/common/item/BookItem.kt
@@ -1,7 +1,10 @@
 package me.alegian.thavma.impl.common.item
 
+import me.alegian.thavma.impl.client.clientSound
 import me.alegian.thavma.impl.client.gui.book.BookScreen
 import me.alegian.thavma.impl.client.setScreen
+import net.minecraft.sounds.SoundEvents
+import net.minecraft.sounds.SoundSource
 import net.minecraft.world.InteractionHand
 import net.minecraft.world.InteractionResultHolder
 import net.minecraft.world.entity.player.Player
@@ -11,7 +14,10 @@ import net.minecraft.world.level.Level
 
 class T7BookItem : Item(Properties().stacksTo(1)) {
   override fun use(level: Level, player: Player, hand: InteractionHand): InteractionResultHolder<ItemStack> {
-    if (level.isClientSide()) setScreen(BookScreen())
+    if (level.isClientSide()) {
+      setScreen(BookScreen())
+      clientSound(SoundEvents.BOOK_PAGE_TURN, SoundSource.AMBIENT, 1f, 1f)
+    }
 
     return InteractionResultHolder.consume(player.getItemInHand(hand))
   }

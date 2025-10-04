67bca76e5dcb123a13f569d056fe949fc52fe833
alegian <alegiannx@gmail.com>
2025-10-04 14:33:45 +0300

    a version that runs

--- src/main/java/me/alegian/thavma/impl/common/level/TreeFelling.kt
+++ src/main/java/me/alegian/thavma/impl/common/level/TreeFelling.kt
@@ -8,18 +8,19 @@ import net.minecraft.world.level.LevelAccessor
 import net.minecraft.world.level.block.state.BlockState
 import net.neoforged.neoforge.event.level.BlockEvent
 import net.neoforged.neoforge.event.tick.LevelTickEvent
-import java.util.ArrayDeque
-import java.util.Deque
+import java.util.*
 
 class TreeFelling(
   val player: ServerPlayer,
   val blockState: BlockState,
-  val positions: Deque<BlockPos>
+  val positions: Queue<BlockPos>
 ) {
   companion object {
     val instances = mutableListOf<TreeFelling>()
+    var disableEvents = false
 
     fun blockBreak(event: BlockEvent.BreakEvent) {
+      if (disableEvents) return
       val player = event.player
       if (player !is ServerPlayer) return
       if (player.isShiftKeyDown) return
@@ -29,46 +30,52 @@ class TreeFelling(
       instances.add(TreeFelling(player, event.state, fellingPositions(event.level, event.pos, event.state)))
     }
 
-    fun fellingPositions(level: LevelAccessor, origin: BlockPos, state: BlockState): Deque<BlockPos> {
-      val checking = ArrayDeque<BlockPos>()
+    fun fellingPositions(level: LevelAccessor, origin: BlockPos, state: BlockState): Queue<BlockPos> {
+      val checking = mutableListOf<BlockPos>()
       checking.add(origin)
       val mutablePos = BlockPos.MutableBlockPos()
-      val visited = ArrayDeque<BlockPos>()
+      val visited = mutableSetOf<BlockPos>()
 
-      while (!checking.isEmpty()) {
-        val currPos = checking.remove()
+      while (!checking.isEmpty() && visited.size < 512) {
+        val currPos = checking.removeFirst()
         visited.add(currPos)
         for (i in -1..1)
           for (j in -1..1)
             for (k in -1..1) {
               if (i == 0 && j == 0 && k == 0) continue
-              mutablePos.set(origin.x + i, origin.y + j, origin.z + k)
+              mutablePos.set(currPos.x + i, currPos.y + j, currPos.z + k)
               if (visited.contains(mutablePos)) continue
               if (state.block != level.getBlockState(mutablePos).block) continue
-              checking.add(mutablePos.immutable())
+              val immutable = mutablePos.immutable()
+              if (checking.contains(immutable)) continue
+              checking.add(immutable)
             }
       }
 
-      return visited
+      return ArrayDeque(visited.sortedBy { v -> v.y })
     }
 
     fun levelTick(event: LevelTickEvent.Pre) {
       if (event.level.isClientSide) return
-      if (event.level.gameTime % 5 != 0L) return
+      if (event.level.gameTime % 2 != 0L) return
 
+      disableEvents = true
       val instanceIterator = instances.iterator()
       while (instanceIterator.hasNext()) {
         val instance = instanceIterator.next()
+        if (instance.player.level() != event.level) continue
         if (
           instance.player.isRemoved ||
-          instance.player.level() != event.level ||
           instance.positions.isEmpty()
         ) {
           instanceIterator.remove()
           continue
         }
-        instance.player.gameMode.destroyBlock(instance.positions.removeLast())
+        val pos = instance.positions.remove()
+        if (event.level.getBlockState(pos).block != instance.blockState.block) continue
+        instance.player.gameMode.destroyBlock(pos)
       }
+      disableEvents = false
     }
   }
 }

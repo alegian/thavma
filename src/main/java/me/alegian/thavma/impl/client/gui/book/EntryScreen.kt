13d3bd5c00baff076bb45073f6fa47256359afd0
KorrAn34 <blazing.grassblock@gmail.com>
2026-05-05 19:19:40 +0200

    Page turning in the Thavmonomicon

--- src/main/java/me/alegian/thavma/impl/client/gui/book/EntryScreen.kt
+++ src/main/java/me/alegian/thavma/impl/client/gui/book/EntryScreen.kt
@@ -1,20 +1,27 @@
 package me.alegian.thavma.impl.client.gui.book
 
 import me.alegian.thavma.impl.client.gui.layout.*
+import me.alegian.thavma.impl.client.gui.research_table.ButtonWidget
 import me.alegian.thavma.impl.client.texture.Texture
 import me.alegian.thavma.impl.common.book.Page
 import me.alegian.thavma.impl.common.research.ResearchEntry
+import me.alegian.thavma.impl.init.registries.deferred.ResearchEntries
+import me.alegian.thavma.impl.rl
 import net.minecraft.client.gui.GuiGraphics
 import net.minecraft.client.gui.screens.Screen
 import net.minecraft.core.Holder
 import net.minecraft.network.chat.Component
+import net.minecraft.resources.ResourceKey
+import net.minecraft.resources.ResourceLocation
+import net.minecraft.world.level.Level
+import kotlin.math.ceil
 
 class EntryScreen(private val entry: Holder<ResearchEntry>) : Screen(Component.literal("Book Entry")) {
   companion object {
     private val BG = Texture("gui/book/background", 510, 282, 512, 512)
   }
 
-  private val currentPage = 0
+  private var currentPage = 0
 
   override fun init() {
     super.init()
@@ -33,17 +40,47 @@ class EntryScreen(private val entry: Holder<ResearchEntry>) : Screen(Component.l
           paddingBottom = 42
           gap = 48
         }) {
+        //for (doublepage in 0..ceil(entry.value().pages.size/2.0).toInt()){
           Row({
             size = grow()
           }) {
             initPage(entry.value().pages.getOrNull(currentPage))
+              Box({
+                  width = fixed(PageTurningWidget.LEFT_TEXTURE.width)
+                  height = fixed(PageTurningWidget.LEFT_TEXTURE.height)
+              }) {
+                  afterLayout {
+                      if (currentPage != 0) {
+                      addRenderableWidget(PageTurningWidget(position, false) {
+                          // reinitiate the screen for this research entry when clicked
+                          // with an updated page
+                          currentPage -= 2
+                          init()
+                      })}
+                  }
+              }
           }
 
           Row({
             size = grow()
           }) {
             initPage(entry.value().pages.getOrNull(currentPage + 1))
+              Box({
+                  width = fixed(PageTurningWidget.RIGHT_TEXTURE.width)
+                  height = fixed(PageTurningWidget.RIGHT_TEXTURE.height)
+              }) {
+                  afterLayout {
+                      if ((entry.value().pages.getOrNull(currentPage + 2) ?: 1) is Page) {
+                      addRenderableWidget(PageTurningWidget(position, true) {
+                          // reinitiate the screen for this research entry when clicked
+                          // with an updated page
+                          currentPage += 2
+                          init()
+                      })}
+                  }
+              }
           }
+        //  }
         }
       }
     }
---
+++ src/main/java/me/alegian/thavma/impl/client/gui/book/PageTurningWidget.kt
@@ -0,0 +1,57 @@
+package me.alegian.thavma.impl.client.gui.book
+
+import com.mojang.blaze3d.systems.RenderSystem
+import me.alegian.thavma.impl.client.gui.research_table.ButtonWidget
+import me.alegian.thavma.impl.client.gui.research_table.ButtonWidget.Companion.LEFT_TEXTURE
+import me.alegian.thavma.impl.client.gui.research_table.ButtonWidget.Companion.leftTranslationId
+import me.alegian.thavma.impl.client.gui.research_table.ButtonWidget.Companion.rightTranslationId
+import me.alegian.thavma.impl.client.texture.Texture
+import me.alegian.thavma.impl.client.util.blit
+import me.alegian.thavma.impl.client.util.translateXY
+import me.alegian.thavma.impl.client.util.usePose
+import me.alegian.thavma.impl.common.block.ResearchTableBlock
+import net.minecraft.client.gui.GuiGraphics
+import net.minecraft.client.gui.components.AbstractWidget
+import net.minecraft.client.gui.components.Tooltip
+import net.minecraft.client.gui.narration.NarrationElementOutput
+import net.minecraft.network.chat.Component
+import net.minecraft.world.phys.Vec2
+
+class PageTurningWidget(position: Vec2, private val invert: Boolean, private val handleClick: () -> Unit) :
+    AbstractWidget(position.x.toInt(), position.y.toInt(), LEFT_TEXTURE.width, LEFT_TEXTURE.height, Component.translatable(if (invert) rightTranslationId else leftTranslationId)) {
+
+    init {
+        tooltip = Tooltip.create(message)
+    }
+
+
+    override fun renderWidget(
+        guiGraphics: GuiGraphics,
+        mouseX: Int,
+        mouseY: Int,
+        partialTick: Float
+    ) {
+        RenderSystem.disableCull()
+        guiGraphics.usePose {
+            translateXY(x, y)
+            val texture = if (invert) ButtonWidget.Companion.RIGHT_TEXTURE else ButtonWidget.Companion.LEFT_TEXTURE
+            guiGraphics.blit(texture)
+        }
+        RenderSystem.enableCull()
+    }
+
+    override fun onClick(mouseX: Double, mouseY: Double, button: Int) {
+        handleClick()
+    }
+
+    companion object {
+        val LEFT_TEXTURE = Texture("gui/research_table/left_button", 35, 12, 35, 12)
+        val RIGHT_TEXTURE = Texture("gui/research_table/right_button", 35, 12, 35, 12)
+        private val namespace = ".buttonWidget"
+        val leftTranslationId = ResearchTableBlock.CONTAINER_TITLE + namespace + ".left"
+        val rightTranslationId = ResearchTableBlock.CONTAINER_TITLE + namespace + ".right"
+    }
+
+    override fun updateWidgetNarration(narrationElementOutput: NarrationElementOutput) {
+    }
+}
